a0315aace92c2f33e17fb2e9f7f96e4d91d8949f
Frank Mori Hess <fmh6jj@gmail.com>
2018-11-18 19:31:12 -0800

    Fluke CDA: add wrappers for doing setup for pyro/sumo

---
+++ setup-angstrom-manifest-pyro.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+exec "$(dirname $(which $0))/setup-angstrom-manifest.sh" "angstrom-v2017.06-yocto2.3-fluke-cda"
---
+++ setup-angstrom-manifest-sumo.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+exec "$(dirname $(which $0))/setup-angstrom-manifest.sh" "angstrom-v2018.06-sumo-fluke-cda"
